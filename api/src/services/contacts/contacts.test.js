7ae6cdfb653031d540b5ded834e28681d96e375e
Billy Priambodo <edo.billy@gmail.com>
2022-01-25 06:31:08 +0700

    delete unnecessary deleted model

--- api/src/services/contacts/contacts.test.js
+++
@@ -1,9 +0,0 @@
-import { contacts } from './contacts'
-
-describe('contacts', () => {
-  scenario('returns all contacts', async (scenario) => {
-    const result = await contacts()
-
-    expect(result.length).toEqual(Object.keys(scenario.contact).length)
-  })
-})
